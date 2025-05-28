b2aa8089df08a3457eb5ce3f726bde1733db616c
Aya-Husien <ayahusien226@gmail.com>
2025-05-28 09:39:28 +0300

    additional seeders

---
+++ database/seeders/SoloTripEventsSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Event;
+use App\Models\SoloTrip;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SoloTripEventsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $soloTrips = SoloTrip::all();
+        $events = Event::all();
+        foreach ($soloTrips as $soloTrip) {
+            $randomEvents = $events->random(rand(2, 4));
+            $attachData = [];
+            foreach ($randomEvents as $event) {
+                $attachData[$event->id] = [
+                    'price' => $event->ticket_price,
+                ];
+            }
+            $soloTrip->events()->attach($attachData);
+
+        }
+    }
+}
