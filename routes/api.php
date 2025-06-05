417da0f45efd1da3d4f828b215d9135f6b5d9a8f
murhaf <www.murhaf2004@gmail.com>
2025-06-05 15:30:56 +0300

    login or register using google

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ Route::get('/user', function (Request $request) {
 Route::post('/generateUnverifiedUser', [AuthController::class, 'generateUnverifiedUser'])->name('generateUnverifiedUser');
 Route::post('/resendVerificationCode', [AuthController::class, 'resendVerificationCode'])->name('resendVerificationCode');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::get('auth/google/redirect', [AuthController::class, 'redirect'])->name('redirectToGoogle');
+Route::get('auth/google/callback', [AuthController::class, 'callback'])->name('loginUsingGoogle');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::middleware(['auth:api-user'])->group(function () {
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
