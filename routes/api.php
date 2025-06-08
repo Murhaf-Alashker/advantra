2c9bf97fe4bcf77f951aacfd21d83f50cb5852a3
Aya-Husien <ayahusien226@gmail.com>
2025-06-08 20:16:45 +0300

    solve route conflicts

--- routes/api.php
+++ routes/api.php
@@ -20,49 +20,49 @@ Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::get('auth/google/redirect', [AuthController::class, 'redirect'])->name('redirectToGoogle');
 Route::get('auth/google/callback', [AuthController::class, 'callback'])->name('loginUsingGoogle');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::middleware(['auth:api-user'])->group(function () {
-    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-});
 
-//
+
+
 Route::prefix('/dashboard')->middleware('auth:api-admin')->group(function () {
     //city api
     Route::controller(CityController::class)->group(function () {
-        Route::post('/countries/{country}/cities/create', 'store');
-        Route::post('/cities/{city}/update', 'update');
+        Route::post('/countries/{country}/cities', 'store')->name('createCity');
+        Route::post('/cities/{city}', 'update')->name('updateCity');
     });
     //event api
     Route::controller(EventController::class)->group(function () {
-        Route::post('/cities/{city}/events/create','store');
-        Route::post('/events/{event:slug}/update','update');
-        Route::delete('/events/{event:slug}','destroy');
+        Route::post('/cities/{city}/events','store')->name('createEvent');
+        Route::post('/events/{event:slug}','update')->name('updateEvent');
+        Route::delete('/events/{event:slug}','destroy')->name('deleteEvent');
     });
     //country api
-    Route::post('/countries/{country}/update', [CountryController::class,'update']);
+    Route::post('/countries/{country}', [CountryController::class,'update'])->name('updateCountry');
 });
 
 Route::middleware('auth:api-user,api-admin,api-guide')->group(function () {
     Route::prefix('/media')->controller(MediaController::class)->group(function () {
-        Route::post('/uploadImages', 'uploadImages');
-        Route::post('/deleteImages', 'deleteImages');
+        Route::post('/uploadImages', 'uploadImages')->name('uploadImages');
+        Route::post('/deleteImages', 'deleteImages')->name('deleteImages');
     });
 });
 
 
 Route::middleware('auth:api-user')->group(function () {
+    //auth api
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 //city api
 Route::controller(CityController::class)->group(function () {
-    Route::get('/cities', 'index');
-    Route::get('/cities/mostEvents','citiesWithMostEvents');
-    Route::get('/cities/{city}', 'show');
-    Route::get('/cities/{city}/events', 'getEvents');
-    Route::get('/cities/{city}/guides','getGuides');
+    Route::get('/cities', 'index')->name('getCities');
+    Route::get('/cities/mostEvents','citiesWithMostEvents')->name('getCitiesWithMostEvents');
+    Route::get('/cities/{city}', 'show')->name('getCity');
+    Route::get('/cities/{city}/events', 'getEvents')->name('getCityEvents');
+    Route::get('/cities/{city}/guides','getGuides')->name('getCityGuides');
 });
 //event api
 Route::controller(EventController::class)->group(function () {
-    Route::get('/events','index');
-    Route::get('/events/{event:slug}','show');
-    Route::get('/events/{event:slug}/relatedEvents','relatedEvents');
-    Route::get('/events/{event:slug}/relatedGuides','relatedGuides');
+    Route::get('/events','index')->name('getEvents');
+    Route::get('/events/{event:slug}','show')->name('getEvent');
+    Route::get('/events/{event:slug}/relatedEvents','relatedEvents')->name('getRelatedEvents');
+    Route::get('/events/{event:slug}/relatedGuides','relatedGuides')->name('getRelatedGuides');
 });
 });
