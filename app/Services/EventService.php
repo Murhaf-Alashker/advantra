5ed271c2822d8d2185b23e2450d047571da8aa85
murhaf <www.murhaf2004@gmail.com>
2025-08-15 12:29:08 +0300

    some changes

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -7,6 +7,7 @@ use App\Models\City;
 use App\Models\Event;
 use App\Models\GroupTrip;
 use App\Models\Guide;
+use App\Models\LimitedEvents;
 use App\Models\Scopes\ActiveScope;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
@@ -41,7 +42,7 @@ class EventService{
 
     public function store(array $data){
        $event = Event::create($data);
-       return $event;
+       return $event->refresh();
     }
 
     public function update(array $data, Event $event){
@@ -119,8 +120,12 @@ class EventService{
         return DB::transaction(function () use ($data, $event) {
             return $event->offers()->create($data);
         });
+    }
 
-
+    public function makeEventLimited(array $info , $eventId):void
+    {
+        $info['event_id'] = $eventId;
+        LimitedEvents::create($info);
     }
 
 }
